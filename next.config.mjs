768923dd1f8cc83a003c3a2722c14c04fbec8ee1
aditya sanjeev <167510875+aperson30@users.noreply.github.com>
2026-07-21 13:04:13 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    // NiiVue is browser-only; prevent bundling Node modulesss
+    // NiiVue is browser-only; prevent bundling Node moduless
     config.resolve.fallback = { fs: false, path: false };
     return config;
   },
