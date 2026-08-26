8fc92592be3b444ca2773b6ff967f4b31a83b650
aditya sanjeev <167510875+aperson30@users.noreply.github.com>
2026-08-26 16:07:14 -0700

    Fix comment typo in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    // NiiVue is browser-only; prevent bundling Node modulesl
+    // NiiVue is browser-only; prevent bundling Node modules
     config.resolve.fallback = { fs: false, path: false };
     return config;
   },
