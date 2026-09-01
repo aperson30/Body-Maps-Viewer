9933d6716d62e49af28339c92e92701d6f757ffe
aditya sanjeev <167510875+aperson30@users.noreply.github.com>
2026-09-01 01:37:54 -0700

    Add comment in webpack configuration
    Added a comment to the webpack configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   webpack: (config) => {
     // NiiVue is browser-only; prevent bundling Node modules
+    //test
     config.resolve.fallback = { fs: false, path: false };
     return config;
   },
